238c7ca739446a1eaa07dbf9664352e6c96f699b
7vTr0x7 <7vTrox7@gmail.com>
2024-12-14 14:56:40 +0530

    model

---
+++ backend/models/recipe.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const RecipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  cuisineType: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Recipe = mongoose.model("Recipe", RecipeSchema);
